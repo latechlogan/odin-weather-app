ea02d7265b4b7382799435aecb161ff26f73269e
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-09-18 20:18:41 -0500

    Refactor dataHandler for object with us & metric

--- src/modules/dataHandler.js
+++ src/modules/dataHandler.js
@@ -1,23 +1,31 @@
 // dataHandler.js
 const dataHandler = (function () {
-  let data = {};
+  let weatherData = {};
 
   const fetchData = async function (location = "71291", unitGroup = "us") {
     let apiUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=${unitGroup}&include=current&key=YPDL4TZQQEGCWFX57LUSCQDWT&contentType=json`;
 
     try {
       const response = await fetch(apiUrl, { method: "GET", headers: {} });
-      data = await response.json();
+      const weather = await response.json();
+      return weather;
     } catch (error) {
       console.error(error);
     }
   };
 
-  const getData = function () {
-    return data;
+  const setWeatherData = function (location) {
+    weatherData = {
+      dataUs: fetchData(location, "us"),
+      dataMetric: fetchData(location, "metric"),
+    };
   };
 
-  return { fetchData, getData };
+  const getWeatherData = function () {
+    return weatherData;
+  };
+
+  return { setWeatherData, getWeatherData };
 })();
 
 export default dataHandler;
