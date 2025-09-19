8135d05f59c7387dcdf41b7debe52a518fd282f9
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-09-19 13:26:25 -0500

    Add init IIFE to start up the app on load

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import "./style.css";
 import inputHandler from "./modules/inputHandler.js";
 import dataHandler from "./modules/dataHandler.js";
 import uiHandler from "./modules/uiHandler.js";
+import eventBus from "./modules/eventBus.js";
 
 window.inputHandler = inputHandler;
 window.dataHandler = dataHandler;
+
+const initApp = (function () {
+  eventBus.emit("appStart");
+})();
