3ed2826043963e1b00e31bd47ee131445df5d162
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-09-18 22:23:39 -0500

    Import uiHandler to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import inputHandler from "./modules/inputHandler.js";
 import dataHandler from "./modules/dataHandler.js";
+import uiHandler from "./modules/uiHandler.js";
 
 window.inputHandler = inputHandler;
 window.dataHandler = dataHandler;
